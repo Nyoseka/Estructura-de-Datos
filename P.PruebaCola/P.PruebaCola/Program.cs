c66f8339e97a15fae54008812653eff2b6950363
Nyoseka <47579115+Nyoseka@users.noreply.github.com>
2019-10-16 12:31:55 -0700

    Programas Prueba Agregado
    Se agregaron los programas prueba de estructuras de datos lineales

---
+++ P.PruebaCola/P.PruebaCola/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P.PruebaCola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> cola1 = new Queue<int>();
+
+            int DatoI;
+            int DatoP;
+            int DatoC;
+
+            Console.Write("Ingrese un priner dato para agregar: ");
+            DatoI = int.Parse(Console.ReadLine());
+            Console.Write("Ingrese un segundo dato: ");
+            DatoP = int.Parse(Console.ReadLine());
+            Console.Write("Inserte un tercer dato: ");
+            DatoC = int.Parse(Console.ReadLine());
+            cola1.Enqueue(DatoI);
+            cola1.Enqueue(DatoP);
+            cola1.Enqueue(DatoC);
+            Console.Clear();
+            Console.WriteLine("Cola"+'\n');
+            Console.WriteLine("Numero de elementos: ",cola1.Count());
+            Console.WriteLine("Elementos dentro de la cola");
+            foreach(int dato in cola1)
+            {
+                Console.Write(dato + " ");
+            }
+            Console.WriteLine("Presione cualquier tecla para continuar");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ P.PruebaLista/P.PruebaLista/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P.PruebaLista
+{
+    class Program
+    {
+        class Figura
+        {
+            public string color;
+            public int poX;
+            public int poY;
+            public Figura()
+            {
+                color = " ";
+                poX = 10;
+                poY = 10;
+            }
+            public Figura(string color, int poX, int poY)
+            {
+                this.color = color;
+                this.poX = poX;
+                this.poY = poY;
+            }
+            public virtual void Dibuja()
+            {
+                Console.WriteLine("Se dibujó una figura de color {0} en la posición: ({1},{2})", color, poX, poY);
+            }
+        }
+        class Rectangulo : Figura
+        {
+            public Rectangulo(string color, int poX, int poY) : base(color, poX, poY)
+            {
+                color = "blanco";
+                poX = 10;
+                poY = 10;
+            }
+            public override void Dibuja()
+            {
+                Console.WriteLine("Se dibujó un rectangulo de color {0} en la posición ({1},{2})", color, poX, poY);
+            }
+        }
+        class Circulo : Figura
+        {
+            public int radio = 10;
+            public Circulo(string color, int poX, int poY) : base(color, poX, poY)
+            {
+                color = "negro";
+                poX = 10;
+                poY = 10;
+            }
+            public override void Dibuja()
+            {
+                Console.WriteLine("Se dibujó un circulo de color {0} en la posición ({1},{2})", color, poX, poY);
+            }
+        }
+        class Triangulo : Figura
+        {
+            public Triangulo(string color, int poX, int poY) : base(color, poX, poY)
+            {
+                color = "blanco";
+                poX = 10;
+                poY = 10;
+            }
+            public override void Dibuja()
+            {
+                Console.WriteLine("Se dibujó un triangulo de color {0} en la posición ({1},{2})", color, poX, poY);
+            }
+        }
+        class Cuadrado : Figura
+        {
+            public Cuadrado(string color, int poX, int poY) : base(color, poX, poY)
+            {
+                color = "blanco";
+                poX = 10;
+                poY = 10;
+            }
+            public override void Dibuja()
+            {
+                Console.WriteLine("Se dibujó un cuadrado de color {0} en la posición ({1},{2})", color, poX, poY);
+            }
+        }
+        static void Main(string[] args)
+        {
+            List<Figura> figuras = new List<Figura>();
+            Rectangulo a = new Rectangulo("rojo", 60, 100);
+            figuras.Add(a);
+            figuras.Add(new Circulo("azul", 52, 160));
+            figuras.Add(new Triangulo("verde", 40, 40));
+            figuras.Add(new Cuadrado("amarillo", 81, 90));
+            foreach (var f in figuras)
+            {
+                f.Dibuja();
+            }
+            Console.ReadKey();
+        }
+    }
+}
