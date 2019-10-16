a641c868a413bcf5b88168200aac80425c759a48
Nyoseka <47579115+Nyoseka@users.noreply.github.com>
2019-10-16 12:33:28 -0700

    Programa Prueba Cola2
    Se ha agregado el programa de Cola2

---
+++ Cola2/Cola2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cola2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> cola = new Queue<string>();
+
+            cola.Enqueue("Perro");
+            cola.Enqueue("Gato");
+            cola.Enqueue("Loro");
+            cola.Enqueue("Tigre");
+            cola.Enqueue("Leon");
+            cola.Enqueue("Lobo");
+            cola.Enqueue("Zorro");
+            cola.Enqueue("Conejo");
+
+            Console.WriteLine("Pila de animales"+'\n');
+            Console.WriteLine("Elementos dentro de la cola: ");
+            foreach(var obj in cola)
+            {
+                Console.WriteLine(obj);
+            }
+            Console.WriteLine('\n'+"   -----   "+'\n');
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Elementos totales que se encuentran en la cola: " + cola.Count);
+
+                Console.WriteLine("Elemento retirado de la cola: " + cola.Dequeue());
+                Console.WriteLine("el proximo elemento que queda en la cola es: " + cola.Peek());
+                Console.WriteLine("");
+                Console.WriteLine("");
+            }
+
+            Console.WriteLine("Elementos totales que se encuentran en la cola: " + cola.Count);
+            Console.WriteLine('\n' + "Presione cualquier tecla para salir");
+            Console.ReadKey();
+        }
+    }
+}
